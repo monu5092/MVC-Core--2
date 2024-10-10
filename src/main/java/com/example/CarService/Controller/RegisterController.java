778c16571c65ab66c4a7441aa4d9fb7f2405e796
monu5092 <monuk1512000@gmail.com>
2024-10-10 22:40:12 +0530

    MVC Core Car Service-2

---
+++ src/main/java/com/example/CarService/Controller/RegisterController.java
@@ -0,0 +1,33 @@
+package com.example.CarService.Controller;
+
+import com.example.CarService.domain.Car;
+import com.example.CarService.domain.Vehicle;
+import com.example.CarService.service.Registration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RegisterController {
+
+    @Autowired
+    Registration registration;
+
+    @RequestMapping("/register")
+    public String getRegistrationPage(Model carModel){
+        Vehicle vehicle=registration.getNewCar();
+        carModel.addAttribute("Vehicle",vehicle);
+        return "carregister";
+    }
+  @RequestMapping("/done")
+  public String getResponsePage(@ModelAttribute("vehicle") Car car) {
+      if (registration.registerCar(car.getRegisterationNumber(), car.getCarName(), car.getCarDetails(), car.getCarWork())) {
+          return "success";
+      } else {
+          return "carregister";
+      }
+  }
+}
